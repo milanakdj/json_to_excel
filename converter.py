e60418cbadf123fcdc8cf93f8f5568d486f6f56b
milanakdj <58652166+milanakdj@users.noreply.github.com>
2025-02-09 22:28:11 +0545

    Add files via upload

--- converter.py
+++ converter.py
@@ -2,7 +2,7 @@ import json
 
 import pandas as pd
 
-with open("list.json", "r") as file:
+with open("list2.json", "r") as file:
     data = json.load(file)
 
 # print(data)
@@ -14,5 +14,5 @@ df = pd.DataFrame(data)
 print(df)
 
 
-df.to_excel('output.xlsx', index=False)
+df.to_excel('output2.xlsx', index=False)
 
