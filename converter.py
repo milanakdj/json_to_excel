97b4bdb6f8ff47385998bcc5dba2e95302a75c7a
milanakdj <58652166+milanakdj@users.noreply.github.com>
2025-02-10 10:02:54 +0545

    Add files via upload

--- converter.py
+++ converter.py
@@ -5,17 +5,30 @@ import pandas as pd
 FILE_NAME = "list3.json"
 OUTPUT_NAME = FILE_NAME.split(".")[0]
 
-with open(FILE_NAME, "r") as file:
-    data = json.load(file)
 
-# print(data)
-json_str = json.dumps(data, indent=4)
+def load_json(file_name):
+    with open(file_name, "r") as file:
+        data = json.load(file)
+    return data
 
-# Print the pretty-printed JSON string
-# print(json_str)
-df = pd.DataFrame(data)
-print(df)
+def convert_to_excel(data):
+    df = pd.DataFrame(data)
+    df.to_excel(f'{OUTPUT_NAME}.xlsx', index=False)
+    return df
+
+def convert(steps):
+    for i, step in enumerate(steps):
+        if i ==0:
+            filename = FILE_NAME
+            data = step(filename)
+        else:
+            data = step(data)   
+    return data
 
 
-df.to_excel(f'{OUTPUT_NAME}.xlsx', index=False)
+if __name__== "__main__":
+    steps = [load_json, convert_to_excel] #uses the global FILE_NAME, and OUTPUT_NAME
+    data = convert(steps)
+    print(data)
+
 
